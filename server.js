87291c50ec9a73c5bf9240a6a4e386e1b56302b8
Stephen Obie <sobie31@outlook.com>
2021-06-16 21:54:08 -0400

    working on function

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
-require("./public/assets/js/apiRoute")(app);
-require("./public/assets/js/htmlIndex")(app);
+require("./routes/apiRoute.js")(app);
+require("./routes/htmlIndex.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on port: http://localhost:${PORT}`);
